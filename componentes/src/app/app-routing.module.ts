d9b63a13503e3fd2c559bc1525b699cdc1745825
Nicolás Ruiz <NICOLASENRIQUERUIZ93@HOTMAIL.COM>
2023-07-18 08:51:33 -0500

    Add header and components

--- componentes/src/app/app-routing.module.ts
+++ componentes/src/app/app-routing.module.ts
@@ -3,20 +3,33 @@ import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
   {
-    path: '',
-    redirectTo: 'folder/Inbox',
-    pathMatch: 'full'
+    path: 'inicio',
+    loadChildren: () =>
+      import('./pages/inicio/inicio.module').then((m) => m.InicioPageModule),
   },
   {
-    path: 'folder/:id',
-    loadChildren: () => import('./folder/folder.module').then( m => m.FolderPageModule)
-  }
+    path: 'alert',
+    loadChildren: () =>
+      import('./pages/alert/alert.module').then((m) => m.AlertPageModule),
+  },
+  {
+    path: 'action-sheet',
+    loadChildren: () =>
+      import('./pages/action-sheet/action-sheet.module').then(
+        (m) => m.ActionSheetPageModule
+      ),
+  },
+  {
+    path: '**',
+    redirectTo: 'inicio',
+    pathMatch: 'full',
+  },
 ];
 
 @NgModule({
   imports: [
-    RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules })
+    RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules }),
   ],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
 export class AppRoutingModule {}
