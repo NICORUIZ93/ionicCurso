82e5047a41e94ecaa80842361d7b53cf0898f37c
Nicolás Ruiz <NICOLASENRIQUERUIZ93@HOTMAIL.COM>
2023-07-24 21:43:30 -0500

    Add proyect Push Notifications

---
+++ Push-Notifications/capacitor.config.ts
@@ -0,0 +1,12 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'Push-Notifications',
+  webDir: 'www',
+  server: {
+    androidScheme: 'https'
+  }
+};
+
+export default config;
