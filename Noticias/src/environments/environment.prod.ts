c9eff3276c9ad2143bc2c79a27904d2e5e188abd
Nicolás Ruiz <NICOLASENRIQUERUIZ93@HOTMAIL.COM>
2023-07-24 21:37:04 -0500

    delete noticias

--- Noticias/src/environments/environment.prod.ts
+++
@@ -1,5 +0,0 @@
-export const environment = {
-  production: true,
-  url: 'https://newsapi.org/v2/everything?q=tesla&from=2023-06-21&sortBy=publishedAt&apiKey=',
-  key: 'e0a48ce3693a46048bd509ebfdd639bc',
-};
